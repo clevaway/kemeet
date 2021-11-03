8e8af0b137a747f2def77132abde633ceb1c15b1
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2021-11-03 15:56:10 +0100

    Minor bugs fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <div class="w-8/12">
         <VideoBox />
       </div>
-      <div class="w-4/12 bg-gray-400 h-screen">
+      <div class="w-4/12 bg-gray-400">
         <ChatBox />
       </div>
     </div>
